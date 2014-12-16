677d576bf634660ae426052bd6257c0ee5e855ca
swapnild-webonise <swapnil.dahiphale@weboniselab.com>
2014-12-16 14:50:44 +0530

    staging commit

---
+++ config/deploy.rb
@@ -0,0 +1,9 @@
+set :application, "myapp"
+set :scm, :git
+set :repository, "git@github.com:swapnild-webonise/cap-deployment.git"
+set :scm_passphrase, ""
+
+set :scm, :subversion
+set :repository, "https://github.com/swapnild-webonise/cap-deployment.git"
+set :user, "deploy"
+
