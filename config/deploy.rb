e22121151ebc16507f4724688c9166e51feb4008
swapnild-webonise <swapnil.dahiphale@weboniselab.com>
2014-12-17 10:23:35 +0530

    test commit

--- config/deploy.rb
+++
@@ -1,9 +0,0 @@
-set :application, "myapp"
-set :scm, :git
-set :repository, "git@github.com:swapnild-webonise/cap-deployment.git"
-set :scm_passphrase, ""
-
-set :scm, :subversion
-set :repository, "https://github.com/swapnild-webonise/cap-deployment.git"
-set :user, "deploy"
-
